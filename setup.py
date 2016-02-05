cf3971d6971a8715c831989b089e1dbd38e70a19
Daniel Lupu <dflupu@bitbucket.org>
2016-02-05 02:20:25 +0200

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tomorrow3",
-    version="1.0.0",
+    version="1.0.1",
     author="Daniel Lupu",
     author_email="lupu.daniel.f@gmail.com",
     packages=find_packages(),
