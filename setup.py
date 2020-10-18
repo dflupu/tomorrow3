05273cac47f214bb302a31d8c89f181f7480f106
Daniel Lupu <dflupu@gmail.com>
2020-10-18 20:03:12 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tomorrow3",
-    version="1.2.0",
+    version="1.2.1",
     author="Daniel Lupu",
     author_email="lupu.daniel.f@gmail.com",
     packages=find_packages(),
